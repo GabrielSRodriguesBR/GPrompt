e09134fcb92b3824dfe55044e448347aedd2b076
GabrielSRodriguesBR <gsrodrigues2@ucs.br>
2020-06-19 00:23:05 -0300

    Add Navs/ message

--- src/GPrompt/js/GPrompt.js
+++ src/GPrompt/js/GPrompt.js
@@ -1,65 +1,114 @@
 
 
-const cursor = '<span class="prompt-cursor"></span>';
-const line = '<div class="line-prompt"><span class="command-prompt"></span></div>';
-var prompt;
-
-function  UpdateText(text){
-    var node_command = document.querySelectorAll(".command-prompt");
-    var last_node = node_command[node_command.length- 1];
-    var textEditor = last_node;
-    var newValue = textEditor.innerHTML + text;
-    textEditor.innerHTML = newValue;
-}
-
-function setNewLine(title){
-    //removendo cursor
-    var c = document.querySelector('.prompt-cursor');
-    if(c != null)
-        c.parentNode.removeChild(c);
-
-    //criando nova linha;
-    var ul = document.querySelector('.command-list-prompt');
-    var li = document.createElement('li'); 
-    li.innerHTML = line;
-    ul.appendChild(li);
-
-    var node_lines = document.querySelectorAll('.line-prompt');
-    var last_node = node_lines[node_lines.length- 1];
-    var editor = last_node;
-    editor.innerHTML = editor.innerHTML + cursor;
-    UpdateText(title);
-}
 
 
-class GPrompt{
-    constructor(text){
-        this.title = text;
-        this.title_line = text+'$ ';
-        document.querySelector('.title-prompt').innerHTML = text;
+function GPrompt(text){
+    this.title = text;
+    this.title_line = text+'$ ';
+    const cursor = '<span class="prompt-cursor"></span>';
+    const line = '<div class="line-prompt"><span class="command-prompt"></span></div>';
+    document.querySelector('.title-prompt').innerHTML = text;
+    setNewLine(this.title_line);
+
+    this.WriteText = async function(text, delay, enter){
+
+        return new Promise(resolve => {
+            var index = 0, text_size = text.length, title = this.title_line;
+
+            function loop(){
+                var char = text[index];
+                UpdateText(char);
+                index++;
+                setTimeout(function(){
+                if(index < text_size){
+                    loop();
+                }
+                else if(enter){
+                    setNewLine(title);
+                    resolve(true);
+                }
+                else{
+                    resolve(true);
+                }
+                },delay);
+            }
+            loop();
+        });
+    }
+
+    function removeCursor(){
+        var c = document.querySelector('.prompt-cursor');
+        if(c != null)
+            c.parentNode.removeChild(c);
+    }
+    function  UpdateText(text){
+        var node_command = document.querySelectorAll(".command-prompt");
+        var last_node = node_command[node_command.length- 1];
+        var textEditor = last_node;
+        var newValue = textEditor.innerHTML + text;
+        textEditor.innerHTML = newValue;
+    }
+    
+    function setNewLine(title){
+        //removendo cursor
+        removeCursor();
+    
+        //criando nova linha;
+        var ul = document.querySelector('.command-list-prompt');
+        var li = document.createElement('li'); 
+        li.innerHTML = line;
+        ul.appendChild(li);
+    
+        var node_lines = document.querySelectorAll('.line-prompt');
+        var last_node = node_lines[node_lines.length- 1];
+        var editor = last_node;
+        editor.innerHTML = editor.innerHTML + cursor;
+        UpdateText(title);
+    }
+
+     this.RunMessage = async function(name, message){
+            await this.WriteText(`./${name}`,100,false);
+            var ul = document.querySelector('.command-list-prompt');
+            var li = document.createElement('li'); 
+            li.className = "g-message";
+            li.innerHTML = message;
+            ul.appendChild(li);
+            setNewLine(this.title_line);
+        
+     }
+
+     this.CreateNav = async function(optionsArray,enter,remove_cursor = false){
+
+        await this.WriteText("cd menu-principal",100,false);
+        this.title_line = this.title_line +'/system/menu-principal$ ';
         setNewLine(this.title_line);
 
-    }
-    WriteText(text, delay, enter){
-        return new Promise(resolve => {
-        var index = 0, text_size = text.length, title = this.title_line;
+        await this.WriteText("ls",300,false);
+        var navUl = document.createElement('ul'); 
+        navUl.className ="g-nav-bar";
+        var navLi;
+        optionsArray.forEach(option => {
+            navLi = document.createElement('li');;
+            navLi.className = "g-nav-item";
+            navLi.setAttribute('id',option.id);
+            navLi.setAttribute('href',option.href);
+            navLi.innerHTML = option.text;
+            navUl.appendChild(navLi);
+        });
 
-        function loop(){
-             var char = text[index];
-             UpdateText(char);
-             index++;
-             setTimeout(function(){
-               if(index < text_size){
-                  loop();
-               }
-               else if(enter){
-                setNewLine(title);
-               }
-            },delay);
+        var ul = document.querySelector('.command-list-prompt');
+        var li = document.createElement('li'); 
+
+        li.appendChild(navUl);
+        ul.appendChild(li);
+
+        if(enter)
+            setNewLine(this.title_line);
+        else if(remove_cursor){
+            removeCursor();
         }
-        loop();
-    });
-        
-       
+
+
+
     }
 }
--- src/GPrompt/js/script.js
+++ src/GPrompt/js/script.js
@@ -1,10 +1,20 @@
 
 
+ 
+document.addEventListener('DOMContentLoaded', async function (event) {
+    var prompt = new GPrompt('gabriel@matrix:');
 
-document.addEventListener('DOMContentLoaded', function (event) {
-     prompt = new GPrompt('gabriel@matrix: ~');
-      prompt.WriteText('Olá, eu sou um prompt :)',100, true);
-      
+    var optionsArray = [
+                        {text: "Portfólio", id: "nav-portfolio", href: "#"}, 
+                        {text: "Sobre Mim", id: "nav-about", href: "#"}, 
+                        {text: "Contato", id: "nav-contact", href: "#"}, 
+                       ];
+    var name = 'apresentacao(1)';
+    var message = 'Olá, meu nome é Gabriel e seja bem vindo ;-)';
+    await prompt.CreateNav(optionsArray, true);
+    // /prompt.RunMessage(name, message);
+   
+    
 });
 
 
